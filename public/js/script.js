0c1ece98bebeec1d2b32d0fdf33387feb03b0cc9
grace <telegrace@gmx.de>
2021-04-07 11:56:39 +0200

    first commit, set up

---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+new Vue({
+	el: "#main-container",
+	data: {
+		name: "Irn Bru",
+		seen: true,
+		cities: [],
+	},
+	mounted: function () {
+		console.log("my main vue instance has mounted!");
+		// we will use axios to communicate with our server
+		console.log("this.cities: ", this.cities);
+		console.log("this: ", this);
+		var self = this;
+		axios
+			.get("/cities")
+			.then(function (response) {
+				console.log("this.cities after axios: ", this.cities);
+				console.log("this after axios: ", this);
+				console.log("response", response.data);
+				console.log("self: ", self);
+				self.cities = response.data;
+			})
+			.catch(function (err) {
+				console.log("error in axios", err);
+			});
+	},
+	methods: {
+		handleClick: function (city) {
+			console.log("handleClick running!!", city);
+			this.seen = !this.seen;
+		},
+	},
+});
